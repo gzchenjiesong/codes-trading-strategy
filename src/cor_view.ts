100b47efce1ddae450522653272ef82c84cedea1
gzchenjiesong <46154915+gzchenjiesong@users.noreply.github.com>
2024-02-02 15:49:41 +0800

    #add new view to show correlation

---
+++ src/cor_view.ts
@@ -0,0 +1,56 @@
+/*
+    Correlation: 相关性系数视图
+    展示多个标的两两间的涨跌走势相关性
+*/
+
+import { TextFileView, WorkspaceLeaf, TFile, TFolder, Vault } from "obsidian";
+
+
+export const VIEW_TYPE_COR = "cor-view"
+
+export class CorView extends TextFileView
+{
+    data_el: HTMLElement;
+
+    constructor(leaf: WorkspaceLeaf)
+    {
+        super(leaf);
+    }
+
+    getViewType(): string
+    {
+        return VIEW_TYPE_COR;
+    }
+
+    getViewData(): string
+    {
+        return this.data;
+    }
+
+    setViewData(data: string, clear: boolean): void 
+    {
+        this.data = data;
+        this.Refresh();
+    }
+
+    clear(): void
+    {
+        this.data = "";    
+    }
+
+    protected async onOpen(): Promise<void> 
+    {
+        let div = this.contentEl.createEl("div");
+        this.data_el = div.createEl("h1");
+    }
+
+    protected async onClose(): Promise<void>
+    {
+        this.contentEl.empty();
+    }
+
+    Refresh()
+    {
+        this.data_el.setText(this.data);
+    }
+}
